0a3da10ea62a3192d2582351b477b5b4291bae80
Alex-is-Perez <perez.alexis.ap25@gmail.com>
2026-09-15 18:16:21 -0500

    Updated pins for UltSnc Python Script

--- Hardware/sensor_test.py
+++ Hardware/sensor_test.py
@@ -7,8 +7,8 @@ from gpiozero import DistanceSensor #so Python can control/read US-sensor
 from time import sleep #to pause between readings
 
 sensor = DistanceSensor(
-    echo=18, #connected to GPIO18
-    trigger=17, #connected to GPIO17
+    echo=18, #connected to GPIO18 - Physical Pin 12
+    trigger=17, #connected to GPIO17 - Physical Pin 11
     max_distance=4 #in meters
 )
 
